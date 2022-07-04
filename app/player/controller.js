0c26a500c7bbc8bee0b424d038830f0f81a5ebe4
nick162 <azwar.hanik@gmail.com>
2022-07-04 14:45:55 +0700

    Update controller.js

--- app/player/controller.js
+++ app/player/controller.js
@@ -326,11 +326,11 @@ module.exports = {
 
   payment: async (req, res) => {
     try {
-      const payment = await Payment.find()
+      const payments = await Payment.find()
         .select("name bankName noRekening type status")
         .populate("banks");
 
-      res.status(200).json({ data: payment });
+      res.status(200).json({ "payments": payments });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
